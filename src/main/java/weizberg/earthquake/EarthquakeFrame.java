41eddc7824940e9c4ca048e4839f4a8830ccbbe6
Alison Weizberg <aweizber@student.touro.edu>
2024-04-14 12:31:42 -0400

    fixed checkstyle

--- src/main/java/weizberg/earthquake/EarthquakeFrame.java
+++ src/main/java/weizberg/earthquake/EarthquakeFrame.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class EarthquakeFrame extends JFrame{
+public class EarthquakeFrame extends JFrame {
     private JList<String> jlist = new JList<>();
 
     public EarthquakeFrame() {
